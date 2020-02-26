2a336a753db50d80f9b8f98026c48e222ae90950
Isla Mukheef <53822275+IslaIDK@users.noreply.github.com>
2020-02-26 22:30:50 +0300

    Update authentication.py

--- Py/Server/Utils/authentication.py
+++ Py/Server/Utils/authentication.py
@@ -1,44 +1,33 @@
-from .AES import decrypt
-
-def authentication(info,key):
-    flag = 0
-    global admin_stat
+def authentication(info):
     user,password = info
-    auth = open('admin.txt','r+')
-    auth = decrypt(auth.read(),key)
-    auth = auth.decode()
-   # for admins in auth: #for admin
-    admins =auth.split(':')
+    auth = open('Utils/admin.txt','r+')
+    admins =(auth.read()).split(':')
+    while('' in admins):
+        admins.remove('')
     for i in range(0,len(admins),2):
         admin = admins[i]
         passwd = admins[1+i]
-        if admin == user and passwd == password:
+        if admin == user and passwd.replace('\n','') == password:
             admin_stat = True
             flag = 1
-            return user
-        else:
-            pass
-    user_auth = open('user.txt','r+')
-    for users in user_auth: #for users
-        users = users.split(':')
-        while('' in users ):
-            users.remove('')
-        #users = user.remove('')
-        name,passwd = users
-
-        if name == user and passwd.replace('\n','') == password:
-            admin_stat = False
-            flag = 1
-            return user
-        else:
-            pass
-
-    try: 
-        if flag == 0:
-            msg = 'User or password did not found'
-            return 0
-        else:
-            pass
-            print('a pass worked')
-    except:
-        print('there was an error while checking the flag for the auth')
+            auth.close()
+            return user , admin_stat
+        else:     
+            user_auth = open('Utils/user.txt','r+')
+            users = (auth.read()).split(':')
+            while('' in users ):
+                users.remove('')
+            name,passwd = users
+            if name == user and passwd.replace('\n','') == password:
+                admin_stat = False
+                flag = 1
+                user_auth.close()
+                return user , admin_stat
+            else:
+                admin_stat = False
+                try:
+                    user_auth.close()
+                    auth.close()
+                except:
+                    pass
+                return 0 , admin_stat
