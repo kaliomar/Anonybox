d9b498f5024410fdbbbc4b570b1f9f991befcbee
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-02-26 14:49:35 +0200

    Update app.py

--- Py/Client/web/app.py
+++ Py/Client/web/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, escape, session , redirect, request
 from flask_socketio import SocketIO
 from flask_wtf.csrf import CSRFProtect,CSRFError
