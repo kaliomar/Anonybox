38dfd269ecc7a562d81fe3e41cda06d2c382fe22
Isla Mukheef <53822275+IslaIDK@users.noreply.github.com>
2020-02-29 15:20:20 +0300

    Update authentication.py

--- Py/Server/Utils/authentication.py
+++ Py/Server/Utils/authentication.py
@@ -4,25 +4,32 @@ def authentication(info):
     admins =(auth.read()).split(':')
     while('' in admins):
         admins.remove('')
-    for i in range(0,len(admins),2):
-        admin = admins[i]
-        passwd = admins[1+i]
-        if admin == user and passwd.replace('\n','') == password:
-            admin_stat = True
-            flag = 1
-            auth.close()
-            return user , admin_stat
-        else:     
-            user_auth = open('Utils/user.txt','r+')
-            users = (auth.read()).split(':')
-            while('' in users ):
-                users.remove('')
-            name,passwd = users
-            if name == user and passwd.replace('\n','') == password:
-                admin_stat = False
+    try:
+        for i in range(0,len(admins),2):
+            admin = admins[i]
+            passwd = admins[1+i]
+            if admin == user and passwd.replace('\n','') == password:
+                admin_stat = True
                 flag = 1
-                user_auth.close()
+                auth.close()
                 return user , admin_stat
+            else:
+                pass         
+        user_auth = open('Utils/user.txt','r+')
+        users = (auth.read()).split(':')
+        while('' in users ):
+            users.remove('')
+    except:
+        return 0 ,0
+    try:
+        for i in range(0,len(users),2):
+            name = users[i]
+            passwd = users[1+i]
+            if name == user and passwd.replace('\n','') == password:
+                    admin_stat = False
+                    flag = 1
+                    user_auth.close()
+                    return user , admin_stat
             else:
                 admin_stat = False
                 try:
@@ -31,3 +38,5 @@ def authentication(info):
                 except:
                     pass
                 return 0 , admin_stat
+    except:
+        return 0 , 0 
