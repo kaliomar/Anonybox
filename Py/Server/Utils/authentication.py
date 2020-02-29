89d688bc4e2b8b28a0b0a98eb876f2b2fb6add22
Isla Mukheef <53822275+IslaIDK@users.noreply.github.com>
2020-02-29 15:45:55 +0300

    Add files via upload

---
+++ Py/Server/Utils/authentication.py
@@ -0,0 +1,44 @@
+def authentication(info):
+    user,password = info
+    auth = open('Utils/admin.txt','r+')
+    admins =(auth.read()).split(':')
+    while('\n' in admins):
+        admins.remove('\n')
+    while('' in admins):
+        admins.remove('')
+    try:
+        for i in range(0,len(admins),2):
+            admin = admins[i]
+            passwd = admins[1+i]
+            if admin == user and passwd.replace('\n','') == password:
+                admin_stat = True
+                flag = 1
+                auth.close()
+                return user , admin_stat
+    except:
+        return 0 ,0
+    try:
+        user_auth = open('Utils/user.txt','r+')
+        users = (user_auth.read()).split(':')
+        while('\n' in users):
+            users.remove('\n')
+        while('' in users ):
+            users.remove('')
+        for i in range(0,len(users),2):
+            name = users[i]
+            passwd = users[1+i]
+            if name == user and passwd.replace('\n','') == password:
+                    admin_stat = False
+                    flag = 1
+                    user_auth.close()
+                    return user , admin_stat
+            else:
+                admin_stat = False
+                try:
+                    user_auth.close()
+                    auth.close()
+                except:
+                    pass
+                return 0 , admin_stat
+    except:
+        return 0 , 0
