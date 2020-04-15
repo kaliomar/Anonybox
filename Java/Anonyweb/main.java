95566ec41b9778ca3c3dadc1d9accd137f0cbb84
Morad Abdelrasheed Mokhtar <48622891+qarqoush@users.noreply.github.com>
2020-04-15 22:29:31 +0200

    DOCS

--- Java/Anonyweb/main.java
+++ Java/Anonyweb/main.java
@@ -83,7 +83,7 @@ public class main {
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890qwertyuiopasdfghjklzxcvbnm";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 3) {
+        while (salt.length() < 20) {
             int index = (int) (rnd.nextFloat() * SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
