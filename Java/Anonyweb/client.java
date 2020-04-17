d1d38840338b5a56dab4dc1da8bfe7ac3b38ae2b
Morad Abdelrasheed Mokhtar <48622891+qarqoush@users.noreply.github.com>
2020-04-17 21:18:30 +0200

    Update client.java

--- Java/Anonyweb/client.java
+++ Java/Anonyweb/client.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
-import javax.swing.JOptionPane;
-
+import javax.swing.*;
 public class client {
 	public static void main(String[] args) {
 	try {
