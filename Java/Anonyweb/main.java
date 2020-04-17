6052e2e981665dd09db89bf8eb1e82df4393ef15
Morad Abdelrasheed Mokhtar <48622891+qarqoush@users.noreply.github.com>
2020-04-17 21:15:31 +0200

    Update main.java

--- Java/Anonyweb/main.java
+++ Java/Anonyweb/main.java
@@ -1,7 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
